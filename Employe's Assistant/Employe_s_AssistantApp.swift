990cd4fe3e0364aa1a38c11c51e45be85d963670
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2023-12-01 19:31:14 +0100

    Initial Commit

---
+++ Employe's Assistant/Employe_s_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Employe_s_AssistantApp.swift
+//  Employe's Assistant
+//
+//  Created by Roman Samborskyi on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Employe_s_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
