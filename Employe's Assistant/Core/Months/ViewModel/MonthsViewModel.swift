bac045bae818025ff277328a755d21da54475547
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2023-12-01 20:44:18 +0100

    fetching months and days

--- Employe's Assistant/Core/Months/ViewModel/MonthsViewModel.swift
+++ Employe's Assistant/Core/Months/ViewModel/MonthsViewModel.swift
@@ -17,6 +17,29 @@ class MonthsViewModel: ObservableObject {
     
     init() { }
     
+    func fetchMonths() {
+        let request = NSFetchRequest<MontEntity>(entityName: coreData.monthsEntety)
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \MontEntity.date, ascending: true)]
+        do {
+            try months = coreData.context.fetch(request)
+        } catch let error {
+            print("Error of fetching months: \(error.localizedDescription)")
+        }
+    }
+    
+    func fetchDays(from month: MontEntity) -> [DayEntity] {
+        let request = NSFetchRequest<DayEntity>(entityName: coreData.dayEntity)
+        request.sortDescriptors = [NSSortDescriptor(keyPath: \DayEntity.date, ascending: false)]
+        request.predicate = NSPredicate(format: "month == @%", month)
+        
+        do {
+            return try coreData.context.fetch(request)
+        } catch let error {
+            print("Error of fetching days: \(error.localizedDescription)")
+            return []
+        }
+    }
+    
     func save() {
         coreData.save()
     }
