af6781891099542f60782159148ac5593c6a933e
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2023-12-01 20:32:21 +0100

    setting app core data container and entities

---
+++ Employe's Assistant/CoreData/CoreDataManager.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataManager.swift
+//  Employe's Assistant
+//
+//  Created by Roman Samborskyi on 01.12.2023.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    static let instanse: CoreDataManager = CoreDataManager()
+    
+    let container: NSPersistentContainer
+    let context: NSManagedObjectContext
+    
+    private let containerName: String = "Container"
+    let monthsEntety: String = "MonthEntity"
+    let dayEntity: String = "DayEntity"
+    let profileEntety: String = "ProfileEntity"
+    
+    init() {
+        container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { NSEntityDescription, error in
+            if error != nil {
+                print("Error of load core data: \(error.debugDescription)")
+            } else {
+                print("SUCCESS of loading core data")
+            }
+        }
+        context = container.viewContext
+    }
+    
+    func save() {
+        do {
+           try context.save()
+        } catch let error {
+            print("Error of saving entity: \(error.localizedDescription)")
+        }
+    }
+}
