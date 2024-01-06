531f28485890451a2824996af7abc0219cc27a8b
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2024-01-06 12:24:34 +0100

    Model structs for monts and days

---
+++ Employe's Assistant/Core/Months/Models/DaysModel.swift
@@ -0,0 +1,20 @@
+//
+//  DaysModel.swift
+//  Employe's Assistant
+//
+//  Created by Roman Samborskyi on 06.01.2024.
+//
+
+import UIKit
+
+
+struct Days {
+    var date: Date
+    var endHours: Int32
+    var endMinutes: Int32
+    var hours: Int32
+    var minutes: Int32
+    var pauseTime: Int32
+    var startHours: Int32
+    var startMinutes: Int32
+}
---
+++ Employe's Assistant/Core/Months/Models/MonthsModel.swift
@@ -0,0 +1,21 @@
+//
+//  MonthsModel.swift
+//  Employe's Assistant
+//
+//  Created by Roman Samborskyi on 06.01.2024.
+//
+
+import UIKit
+
+
+struct Months {
+    var date: Date
+    var monthTarget: Int32
+    var title: String
+    var totalHours: Double
+    var totalSalary :Double
+    var trim: Double
+    var days: [Days]
+}
+
+
