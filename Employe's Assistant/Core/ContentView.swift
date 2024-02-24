2cef62d81e0e74575c678fbb4874f1872e3459ee
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2024-02-24 10:25:59 +0100

    some commit

--- Employe's Assistant/Core/ContentView.swift
+++ Employe's Assistant/Core/ContentView.swift
@@ -29,6 +29,7 @@ struct ContentView: View {
                 .tabItem {
                     Label("Settings", systemImage: "gear")
                 }
+            Text("sldflks")
         }.accentColor(svm.newAccentColor)
     }
 }
