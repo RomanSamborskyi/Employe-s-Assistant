75b1edb4d1711be3ca11d84ae3eaa1a6ba2271a0
Roman Samborskyi <82052893+RomanSamborskyi@users.noreply.github.com>
2024-03-25 19:28:41 +0100

    Added onboarding directory

---
+++ Employe's Assistant/Core/OnboardingViews/OnboardingMainView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingMainView.swift
+//  Employee's Assistant
+//
+//  Created by Roman Samborskyi on 25.03.2024.
+//
+
+import SwiftUI
+
+struct OnboardingMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingMainView()
+}
